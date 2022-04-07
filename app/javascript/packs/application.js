af0aac73db5b0cdc01be934bd42eb67003ed2b01
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-07 21:41:59 +0700

    added validations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,26 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// Not sure which one of these to use
-require("bootstrap/dist/js/bootstrap")
+// https://matthewhoelter.com/2021/08/13/how-to-install-bootstrap-5-in-ruby-on-rails-6-with-webpack.html
+require("@popperjs/core")
 import "bootstrap"
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
+// The stylesheet location we created earlier
+require("../../assets/stylesheets/application.scss")
+
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,9 @@
 const { environment } = require('@rails/webpacker')
+
 const webpack = require('webpack')
 
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
