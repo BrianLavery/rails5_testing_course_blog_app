9ee36b2a33868a4a010b501ea346dcad065172ce
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-09 15:16:23 +0700

    hide edit delete links from non signed in users and non owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -32,16 +32,16 @@ RSpec.feature 'Creating Articles' do
     expect(page).not_to have_link("Delete Article")
   end
 
-  # scenario 'to non owner hide Edit and Delete buttons' do
-  #   login_as(@fred)
-  #   visit root_path
-  #   click_link @article.title
+  scenario 'signed in owner sees both edit and delete buttons' do
+    login_as(@john)
+    visit root_path
+    click_link @article.title
 
-  #   expect(page).to have_content(@article.title)
-  #   expect(page).to have_content(@article.body)
-  #   expect(current_path).to eq(article_path(@article))
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
 
-  #   expect(page).not_to have_link("Edit Article")
-  #   expect(page).not_to have_link("Delete Article")
-  # end
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
 end
